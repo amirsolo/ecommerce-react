6aa564dbd9447a89250ae299d212dde620fb8285
Amir Askari <theamirsolo@gmail.com>
2021-03-01 17:25:27 +0330

    add react-router-dom and setup basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import './App.css'
+
+const Hat = () => {
+  return <div>HAT PAGE</div>
+}
 
 function App() {
   return (
     <div className='App'>
-      <div>Hello World !</div>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/hats' component={Hat} />
+      </Switch>
     </div>
   )
 }
